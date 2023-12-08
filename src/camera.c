7172e5d04a316175a5675b0f069cfb56aa0a4352
GhettoBastler <100698182+GhettoBastler@users.noreply.github.com>
2023-12-08 09:31:35 +0100

    added homogeneous coordinates operations to camera.c

--- src/camera.c
+++ src/camera.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "camera.h"
 
@@ -101,4 +102,69 @@ void update_rotation_matrix(Camera* pcam){
     pcam->rot_mat[6] = -sinf(r.y);
     pcam->rot_mat[7] = cosf(r.y) * sinf(r.z);
     pcam->rot_mat[8] = cosf(r.y) * cosf(r.z);
+}
+
+// Homogeneous coordinates transforms
+Point3D transform_point(float* matrix, Point3D point){
+    Point3D res;
+    res.x = point.x * matrix[0] + point.y * matrix[1] + point.z * matrix[2] + matrix[3];
+    res.y = point.x * matrix[4] + point.y * matrix[5] + point.z * matrix[6] + matrix[7];
+    res.z = point.x * matrix[8] + point.y * matrix[9] + point.z * matrix[10] + matrix[11];
+    return res;
+}
+
+void calculate_transform_matrix(float* matrix,
+                                float r_x, float r_y, float r_z, 
+                                float t_x, float t_y, float t_z){
+
+    float r0, r1, r2, r3, r4, r5, r6, r7, r8;
+
+    r0 = cosf(r_x) * cosf(r_y);
+    r1 = cosf(r_x) * sinf(r_y) * sinf(r_z) - sinf(r_x) * cosf(r_z);
+    r2 = cosf(r_x) * sinf(r_y) * cosf(r_z) + sinf(r_x) * sinf(r_z);
+    r3 = sinf(r_x) * cosf(r_y);
+    r4 = sinf(r_x) * sinf(r_y) * sinf(r_z) + cosf(r_x) * cosf(r_z);
+    r5 = sinf(r_x) * sinf(r_y) * cosf(r_z) - cosf(r_x) * sinf(r_z);
+    r6 = -sinf(r_y);
+    r7 = cosf(r_y) * sinf(r_z);
+    r8 = cosf(r_y) * cosf(r_z);
+
+    matrix[0] = r0;
+    matrix[1] = r1;
+    matrix[2] = r2;
+    matrix[4] = r3;
+    matrix[5] = r4;
+    matrix[6] = r5;
+    matrix[8] = r6;
+    matrix[9] = r7;
+    matrix[10] = r8;
+
+    matrix[3] = r0 * t_x + r1 * t_y + r2 * t_z;
+    matrix[7] = r3 * t_x + r4 * t_y + r5 * t_z;
+    matrix[11] = r6 * t_x + r7 * t_y + r8 * t_z;
+
+    matrix[12] = matrix[13] = matrix[14] = 0;
+    matrix[15] = 1;
+}
+
+void multiply_matrix(float* matA, float* matB){
+    float res[16];
+    res[0] = matA[0] * matB[0] + matA[1] * matB[4] + matA[2] * matB[8] + matA[3] * matB[12];
+    res[1] = matA[0] * matB[1] + matA[1] * matB[5] + matA[2] * matB[9] + matA[3] * matB[13];
+    res[2] = matA[0] * matB[2] + matA[1] * matB[6] + matA[2] * matB[10] + matA[3] * matB[14];
+    res[3] = matA[0] * matB[3] + matA[1] * matB[7] + matA[2] * matB[11] + matA[3] * matB[15];
+    res[4] = matA[4] * matB[0] + matA[5] * matB[4] + matA[6] * matB[8] + matA[7] * matB[12];
+    res[5] = matA[4] * matB[1] + matA[5] * matB[5] + matA[6] * matB[9] + matA[7] * matB[13];
+    res[6] = matA[4] * matB[2] + matA[5] * matB[6] + matA[6] * matB[10] + matA[7] * matB[14];
+    res[7] = matA[8] * matB[3] + matA[5] * matB[7] + matA[6] * matB[11] + matA[7] * matB[15];
+    res[4] = matA[8] * matB[0] + matA[9] * matB[4] + matA[10] * matB[8] + matA[11] * matB[12];
+    res[5] = matA[8] * matB[1] + matA[9] * matB[5] + matA[10] * matB[9] + matA[11] * matB[13];
+    res[6] = matA[8] * matB[2] + matA[9] * matB[6] + matA[10] * matB[10] + matA[11] * matB[14];
+    res[7] = matA[8] * matB[3] + matA[9] * matB[7] + matA[10] * matB[11] + matA[11] * matB[15];
+    res[4] = matA[12] * matB[0] + matA[13] * matB[4] + matA[14] * matB[8] + matA[15] * matB[12];
+    res[5] = matA[12] * matB[1] + matA[13] * matB[5] + matA[14] * matB[9] + matA[15] * matB[13];
+    res[6] = matA[12] * matB[2] + matA[13] * matB[6] + matA[14] * matB[10] + matA[15] * matB[14];
+    res[7] = matA[12] * matB[3] + matA[13] * matB[7] + matA[14] * matB[11] + matA[15] * matB[15];
+
+    memcpy(matA, res, sizeof(float) * 16);
 }
