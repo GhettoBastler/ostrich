4aa25dfdfe1b5f80162137d74adfae2a124659bd
GhettoBastler <100698182+GhettoBastler@users.noreply.github.com>
2023-12-13 14:39:51 +0100

    added scale

--- src/scene.c
+++ src/scene.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
+#include "vect.h"
 #include "transforms.h"
 #include "primitives.h"
 #include "stl.h"
 
 #define STL_FILE "input.stl"
+#define SCALE 20
 
 TriangleMesh* tri_make_scene(){
     FILE* pfile = fopen(STL_FILE, "r");
@@ -13,6 +15,14 @@ TriangleMesh* tri_make_scene(){
         exit(1);
     }
     TriangleMesh* pscene = stl_to_tri_mesh(pfile);
+
+    // Scale model
+    for (int i = 0; i < pscene->size; i++){
+        pscene->triangles[i].a = pt_mul(SCALE, pscene->triangles[i].a);
+        pscene->triangles[i].b = pt_mul(SCALE, pscene->triangles[i].b);
+        pscene->triangles[i].c = pt_mul(SCALE, pscene->triangles[i].c);
+    }
+
     fclose(pfile);
     return pscene;
 }
