e8436fd0ed93f6c69b53bf5c0441f176c67455fd
GhettoBastler <100698182+GhettoBastler@users.noreply.github.com>
2023-12-18 20:38:26 +0100

    moved point_is_visible

--- src/cull_clip.h
+++ src/cull_clip.h
@@ -4,6 +4,7 @@
 #include "camera.h"
 #include "primitives.h"
 
+bool point_is_visible(Edge3D edge, float ratio, TriangleMesh* ptri_mesh, int start_idx);
 void clip_frustum(Edge3D* edge, Camera* pcam);
 TriangleMesh* bface_cull(float* matrix, TriangleMesh* ptri);
 bool point_is_visible(Edge3D edge, float ratio, TriangleMesh* pmesh, int start_idx);
