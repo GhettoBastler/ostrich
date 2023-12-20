11c270cee23290272f097ff465273833fea36bbe
GhettoBastler <100698182+GhettoBastler@users.noreply.github.com>
2023-12-20 14:16:46 +0100

    define icon dimensions

--- src/ui.c
+++ src/ui.c
@@ -2,21 +2,34 @@
 #include "icons.c"
 #include "ui.h"
 
+#define ICON_WIDTH 50
+#define ICON_HEIGHT 50
+#define BAR_HEIGHT 70
+#define SPACING 30
+#define MARGIN 10
+
 static SDL_Texture* ptexture_icons;
 static SDL_Rect ui_bg_rect;
-static SDL_Rect camera_dst_rect;
-static SDL_Rect hlr_dst_rect;
-const SDL_Rect orbit_src_rect = {0, 0, 50, 50};
-const SDL_Rect eye_src_rect = {0, 50, 50, 50};
-const SDL_Rect hlr_on_src_rect = {50, 0, 50, 50};
-const SDL_Rect hlr_off_src_rect = {50, 50, 50, 50};
+static SDL_Rect camera_dst_rect = {MARGIN,
+                                   MARGIN,
+                                   ICON_WIDTH,
+                                   ICON_HEIGHT};
+static SDL_Rect hlr_dst_rect = {MARGIN + ICON_WIDTH + SPACING,
+                                MARGIN,
+                                ICON_WIDTH,
+                                ICON_HEIGHT};
+
+const SDL_Rect orbit_src_rect = {0, 0, ICON_WIDTH, ICON_HEIGHT};
+const SDL_Rect eye_src_rect = {0, ICON_HEIGHT, ICON_WIDTH, ICON_HEIGHT};
+const SDL_Rect hlr_on_src_rect = {ICON_WIDTH, 0, ICON_WIDTH, ICON_HEIGHT};
+const SDL_Rect hlr_off_src_rect = {ICON_WIDTH, ICON_HEIGHT, ICON_WIDTH, ICON_HEIGHT};
 
 void init_ui(int win_height, int win_width, SDL_Renderer* prenderer){
     // Background
     ui_bg_rect.x = 0;
-    ui_bg_rect.y = win_height-70;
+    ui_bg_rect.y = win_height-BAR_HEIGHT;
     ui_bg_rect.w = win_width;
-    ui_bg_rect.h = 70;
+    ui_bg_rect.h = BAR_HEIGHT;
 
     // Icons
     // Load sheet
@@ -30,15 +43,11 @@ void init_ui(int win_height, int win_width, SDL_Renderer* prenderer){
     free(psurface_icons);
 
     // Set render positions
-    camera_dst_rect.x = ui_bg_rect.x + 10;
-    camera_dst_rect.y = ui_bg_rect.y + 10;
-    camera_dst_rect.w = 50;
-    camera_dst_rect.h = 50;
+    camera_dst_rect.x += ui_bg_rect.x;
+    camera_dst_rect.y += ui_bg_rect.y;
 
-    hlr_dst_rect.x = ui_bg_rect.x + 70;
-    hlr_dst_rect.y = ui_bg_rect.y + 10;
-    hlr_dst_rect.w = 50;
-    hlr_dst_rect.h = 50;
+    hlr_dst_rect.x += ui_bg_rect.x;
+    hlr_dst_rect.y += ui_bg_rect.y;
 };
 
 void draw_ui(SDL_Renderer* prenderer, bool orbit_mode, bool hidden_removed){
